18144de9513327fcca3e190ddf64692aa051cf82
Farrelmadyasthawidyadhana <138749807+Fantasymind@users.noreply.github.com>
2025-04-26 22:20:39 +0700

    Update ceklimit.py

--- ceklimit.py
+++ ceklimit.py
@@ -4,7 +4,7 @@ import json
 response = requests.get(
   url="https://openrouter.ai/api/v1/auth/key",
   headers={
-    "Authorization": f"Bearer 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"
+    "Authorization": f"Bearer busssssss"
   }
 )
 
